c5daa6b0853a71ab6d9c4ce956728cc5e1365fd3
Rohan Chavda <rohan.chavda@securiti.ai>
2024-07-17 17:31:09 +0530

    added zip and json for carthage

---
+++ Carthage/Checkouts/TestLibrary317/TestLibrary317.xcframework/ios-arm64_x86_64-simulator/TestLibrary317.framework/Headers/TestLibrary317.h
@@ -0,0 +1,18 @@
+//
+//  TestLibrary317.h
+//  TestLibrary317
+//
+//  Created by Rohan Chavda on 17/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestLibrary317.
+FOUNDATION_EXPORT double TestLibrary317VersionNumber;
+
+//! Project version string for TestLibrary317.
+FOUNDATION_EXPORT const unsigned char TestLibrary317VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLibrary317/PublicHeader.h>
+
+
